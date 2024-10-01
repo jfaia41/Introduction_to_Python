139e4dc966541e9620fe958f2f285c422e779e41
jfaia41 <joao.colarinha41@gmail.com>
2024-10-01 23:01:41 +0100

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,20 @@
+# Prompts the user for a file name, removes any leading or trailing whitespace, and converts all characters to lowercase
+file_name = input("File name: ").strip().lower()
+
+
+if file_name.endswith(".gif"):
+    print("image/gif")
+elif file_name.endswith(".jpg"):
+    print("image/jpeg")
+elif file_name.endswith(".jpeg"):
+    print("image/jpeg")
+elif file_name.endswith(".png"):
+    print("image/png")
+elif file_name.endswith(".pdf"):
+    print("application/pdf")
+elif file_name.endswith(".txt"):
+    print("text/plain")
+elif file_name.endswith(".zip"):
+    print("application/zip")
+else:
+     print("application/octet-stream")
