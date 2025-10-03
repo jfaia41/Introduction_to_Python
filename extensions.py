bd191372ee10d904ccaa7061c49b42a7d512e319
jfaia41 <joao.colarinha41@gmail.com>
2025-10-03 16:28:54 +0100

    extensions update

--- extensions.py
+++ extensions.py
@@ -1,20 +1,28 @@
-# Prompts the user for a file name, removes any leading or trailing whitespace, and converts all characters to lowercase
-file_name = input("File name: ").strip().lower()
+# Perguntar ao utilizador o tipo do ficheiro - pedir um "input"
+# Lista: .gif; .jpg; .jpeg; .png; .pdf; .txt; .zip
+
+file_name = input('File_name:')
+file_name = file_name.lower().strip()
 
 
-if file_name.endswith(".gif"):
-    print("image/gif")
-elif file_name.endswith(".jpg"):
-    print("image/jpeg")
-elif file_name.endswith(".jpeg"):
-    print("image/jpeg")
-elif file_name.endswith(".png"):
-    print("image/png")
-elif file_name.endswith(".pdf"):
-    print("application/pdf")
-elif file_name.endswith(".txt"):
-    print("text/plain")
-elif file_name.endswith(".zip"):
-    print("application/zip")
+# Escrever o tipo de ficheiro e a sua extensão, p.e. "image/.jpeg"
+# Se não for um tipo que esteja na lista, escrever "application/octet-stream"
+
+if file_name.endswith ('.gif'):
+    print ('image/gif')
+elif file_name.endswith ('.jpg'):
+    print ('image/jpeg')
+elif file_name.endswith ('.jpeg'):
+    print ('image/jpeg')
+elif file_name.endswith ('.png'):
+    print ('image/png')
+elif file_name.endswith ('.pdf'):
+    print ('application/pdf')
+elif file_name.endswith ('.txt'):
+    print ('text/plain')
+elif file_name.endswith ('.zip'):
+    print ('application/zip')
 else:
-     print("application/octet-stream")
+    print ('application/octet-stream')
+
+
