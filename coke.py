7f7ff7e658e439a4c4bed9017758f48511c0d1b4
jfaia41 <joao.colarinha41@gmail.com>
2025-10-03 17:41:40 +0100

    coke update

--- coke.py
+++ coke.py
@@ -1,34 +1,27 @@
-# Cost of the coke
+# Custo da coca-cola = 50
 cost = 50
-amount_due = cost
 
+# Conta = custo
+acount = cost
 
-# Lista of values
-accepted_coins = [25, 10, 5]
+# Calcular o troco
+while acount > 0:
+    print (f"Amount Due: {acount}")
+    money = int(input("Insert Coin:"))
+    
 
-# Function that shows the amount still owed
-def display_amount_due(amount_due):
-    print(f"Amount Due: {amount_due}")
+# Só aceita moedas de 25, 10, ou 5
 
-# Prompt the user until the total inserted amount reaches or exceeds the cost
+    if money in [25, 10, 5]:
+        acount-=money
 
-while amount_due > 0:
+    else:
+        print("Invalid coin. Please insert 25, 10, or 5.")
 
-    display_amount_due(amount_due) # Call the function to show the amount owed
+# Indicar o valor do troco
+## Variável troco
 
-    try:
-        # Prompt the user to insert a coin
-        coin = int(input("Insert Coin: "))
-
-        # Check if the inserted coin is one of the accepted denominations
-        if coin in accepted_coins:
-            amount_due -= coin
-        else:
-            print(f"Invalid coin. Please insert 25, 10, or 5 cents only.")
-
-    except ValueError:
-        print("Invalid input. Please insert an integer.")
-
-# Calculate change if any
-change = abs(amount_due)
-print(f"Change Owed: {change}")
+change = 0
+if acount < money:
+    change = abs(acount)
+    print(f"Change Owed: {change}")
